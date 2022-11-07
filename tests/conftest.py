ad7cfce7b501e89366de63d81c8c9c0acc1a8f23
James Lafferty <jlaf85@gmail.com>
2022-11-06 18:52:52 -0800

    add fix for db_path in def app()

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
 
 @pytest.fixture
 def app():
-    db_fd, db_path = template.mkstemp()
+    db_fd, db_path = tempfile.mkstemp()
 
     app = create_app({
         'TESTING': True,
